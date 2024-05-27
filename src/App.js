c27488acc2a5f8bf654ca8b0d68c74eef751740f
MajaSentin <maja.ratkovic.ns@gmail.com>
2024-05-27 14:55:46 +0200

    Races base setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Teams from "./components/Teams/Teams";
 import Drivers from './components/Drivers/Drivers';
+import Races from "./components/Races/Races";
 
 function App() {
   return (
     <>
       <Teams />
       <Drivers />
+      <Races />
     </>
   );
 }
