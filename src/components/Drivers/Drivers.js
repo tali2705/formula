b2744b18d3b7b03eaba8fbc59935733a5ac0fb65
tali2705 <natalija8140@gmail.com>
2024-05-28 10:52:36 +0200

    deleting comments

--- src/components/Drivers/Drivers.js
+++ src/components/Drivers/Drivers.js
@@ -28,37 +28,6 @@ const Drivers = () => {
     };
 
     return (
-        // <table>
-        //     <caption>Constructors for Championship Standings - 2023</caption>
-        //     <thead>
-        //         <tr>
-        //             <th>Position</th>
-        //             <th>Constructor</th>
-        //             <th>Points</th>
-        //         </tr>
-        //     </thead>
-        //     <tbody>
-        //         {drivers.length > 0 ? (
-        //             drivers.map((driver) => (
-        //                 const countryCode = nationalityToCountryCode(
-        //                 driver.Driver.nationality);
-        //         <tr key={driver.position}>
-        //             <td>{driver.position}</td>
-        //             <td><img
-        //                 src={`https://flagsapi.com/${countryCode}/shiny/64.png`}
-        //                 alt={countryCode} />&nbsp;{driver.Driver.givenName}&nbsp;{driver.Driver.familyName}</td>
-        //             <td>{driver.Constructors.length > 0 &&
-        //                 driver.Constructors[0].name}</td>
-        //             <td>{driver.points}</td>
-        //         </tr>
-
-        //         ))
-
-        //         ) : (
-        //         <p>Loading teams...</p>
-        //         )}
-        //     </tbody>
-        // </table>
         <table>
             <caption>
 
