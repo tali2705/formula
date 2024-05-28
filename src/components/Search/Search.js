8483c45abea42959647c565761f970bf3f85ccdd
marijanagrujcic <marijanagrujicic111@gmail.com>
2024-05-28 13:22:41 +0200

    card setup

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
-import axios from "axios";
 
 const Search = () => {
     const [inputValue, setInputValue] = useState("");
 
     useEffect(() => {
         setInputValue();
-    })
+    });
 
     const handleChange = (e) => {
         console.log(e);
@@ -17,9 +16,10 @@ const Search = () => {
 
     return (
         <div>
-           <input type="text" placeholder="Search..." value={inputValue} onChange={(e) => handleChange(e.target.value)}/>
+            <input type="text" placeholder="Search..." value={inputValue} onChange={(e) => handleChange(e.target.value)} />
         </div>
     )
 }
 
 export default Search;
+
