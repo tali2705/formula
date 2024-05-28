bbde3a8d9ee06df8bc404045f9fa9a3318fedc6b
Ferenc Koler <ferenckoler@outlook.com>
2024-05-28 11:50:11 +0200

    RaceResult added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Drivers from './components/Drivers/Drivers';
 import Search from './components/Search/Search';
 import Races from './components/Races/Races';
 import QualifyingResults from './components/QualifyingResults.js/QualifyingResults';
+import RaceResult from './components/RaceResult/RaceResult';
 
 function App() {
     return (
         <>
             {/* <Search /> */}
             {/* <Teams /> */}
-            <Drivers />
+            {/* <Drivers /> */}
             {/* <Races /> */}
-            <QualifyingResults />
+            {/* <QualifyingResults /> */}
+            <RaceResult />
         </>
     );
 }
