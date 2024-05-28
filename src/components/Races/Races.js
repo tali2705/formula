018e00f251ddcdb0763a0ace0f18325cf3609e6c
tali2705 <natalija8140@gmail.com>
2024-05-28 12:37:59 +0200

    Races setup table

--- src/components/Races/Races.js
+++ src/components/Races/Races.js
@@ -1,47 +1,64 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './races.scss';
+import Loader from '../Loader';
 
 const Races = () => {
     const [races, setRaces] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        const getRaces = async () => {
-            const url = 'http://ergast.com/api/f1/2023/results/1.json';
-
-            try {
-                const response = await axios.get(url);
-                console.log(response.data);
-
-                const raceStandings = response.data.MRData.RaceTable.Races;
-
-                setRaces(raceStandings);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
         getRaces();
-
-        console.log('UseEffect getRaces');
     }, []);
 
-    console.log(races);
+    const getRaces = async () => {
+        const url = 'http://ergast.com/api/f1/2013/results/1.json';
 
+        try {
+            const response = await axios.get(url);
+            // console.log(response.data);
+
+            const raceStandings = response.data.MRData.RaceTable.Races;
+            setRaces(raceStandings);
+            setIsLoading(false);
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    if (isLoading) {
+        return <Loader />
+    }
     return (
-        <div>
-            {races.length > 0 ? (
-                <ul>
-                    {races.map((race, index) => (
-                        <li key={index}>
-                            {race.raceName} - {race.season}
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>Loading races...</p>
-            )}
-        </div>
+        <table>
+            <caption>Race calendar - 2013</caption>
+            <thead>
+                <tr>
+                    <th>Round</th>
+                    <th>Grand Prix</th>
+                    <th>Circuit</th>
+                    <th>Date</th>
+                    <th>Winner</th>
+                </tr>
+            </thead>
+            <tbody>
+                {races.length > 0 ? (
+                    races.map((race) => (
+                        <tr key={race.round}>
+                            <td>{race.round}</td>
+                            <td>{race.raceName}</td>
+                            <td>{race.Circuit.circuitName}</td>
+                            <td>{race.date}</td>
+                            <td>{race.Results[0].Driver.familyName}</td>
+                        </tr>
+                    ))
+                ) : (
+                    <p>Loading races...</p>
+                )
+                }
+            </tbody>
+        </table >
     );
 };
 
