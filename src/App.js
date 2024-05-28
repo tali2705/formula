ed25aff4f5e11065a4fa483d67da359b213c7fee
Ferenc Koler <ferenckoler@outlook.com>
2024-05-28 13:20:04 +0200

    Drivers Pix added

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
             {/* <Teams /> */}
             <Drivers />
             {/* <Races /> */}
-            <QualifyingResults />
+            {/* <QualifyingResults /> */}
         </>
     );
 }
