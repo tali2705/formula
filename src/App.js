bba38fe336bd6cf36ffe7475a3f09b80da9b954f
Ferenc Koler <ferenckoler@outlook.com>
2024-05-29 15:00:48 +0200

    Bug Fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import SideBar from './components/SideBar/SideBar';
-<<<<<<< HEAD
-import Header from './components/Header/Header';
-=======
 
->>>>>>> c4e8d53d809672e8cfb8fc5b26c24b7d7207f840
 function App() {
-
     return (
         <>
-<<<<<<< HEAD
-            <Header />
-=======
->>>>>>> c4e8d53d809672e8cfb8fc5b26c24b7d7207f840
             <SideBar />
         </>
     );
