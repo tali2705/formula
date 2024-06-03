4f17a52052416d55421524712344ccfeeb33b271
Ferenc Koler <ferenckoler@outlook.com>
2024-06-03 11:23:13 +0200

    TS Stage #2

--- src/components/Races/RaceRow.tsx
+++ src/components/Races/RaceRow.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import flagHandler from '../utils/flagHandler';
+
 import { IRaceRowProps } from '../Interfaces/GlobalInterface';
 
 const RaceRow: React.FC<IRaceRowProps> = ({ race }) => {
