34ca89a048687ed1302bb105a3c206701262b8cc
Ferenc Koler <ferenckoler@outlook.com>
2024-06-04 20:58:44 +0200

    Migrating to TypeScript #1

--- src/components/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer: React.FC = () => (
-    <div className='footer'>
-        <p>&copy; Copyright Company Name 2024.</p>
-    </div>
-);
-
-export default Footer;
