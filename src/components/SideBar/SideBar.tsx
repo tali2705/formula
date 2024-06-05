d9f8e2b900c33441bf85d1f97ee20d5427f7c336
tali2705 <natalija8140@gmail.com>
2024-06-06 00:16:59 +0200

    logo as link added

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,54 +1,56 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const SideBar: React.FC = () => {
-    return (
-        <div className='sidebar-wrapper'>
-            <img
-                className='logo'
-                src={require('../../assets/FI-logo_rendered.png')}
-                alt='logo'
-            />
-            <nav className='side-nav'>
-                <NavLink
-                    to='/drivers'
-                    className={({
-                        isActive,
-                        isPending,
-                    }: {
-                        isActive: boolean;
-                        isPending: boolean;
-                    }) => (isPending ? 'pending' : isActive ? 'active' : '')}
-                >
-                    Drivers
-                </NavLink>
-                <NavLink
-                    to='/teams'
-                    className={({
-                        isActive,
-                        isPending,
-                    }: {
-                        isActive: boolean;
-                        isPending: boolean;
-                    }) => (isPending ? 'pending' : isActive ? 'active' : '')}
-                >
-                    Teams
-                </NavLink>
-                <NavLink
-                    to='/races'
-                    className={({
-                        isActive,
-                        isPending,
-                    }: {
-                        isActive: boolean;
-                        isPending: boolean;
-                    }) => (isPending ? 'pending' : isActive ? 'active' : '')}
-                >
-                    Races
-                </NavLink>
-            </nav>
-        </div>
-    );
+  return (
+    <div className="sidebar-wrapper">
+      <NavLink to="/">
+        <img
+          className="logo"
+          src={require("../../assets/FI-logo_rendered.png")}
+          alt="logo"
+        />
+      </NavLink>
+      <nav className="side-nav">
+        <NavLink
+          to="/drivers"
+          className={({
+            isActive,
+            isPending,
+          }: {
+            isActive: boolean;
+            isPending: boolean;
+          }) => (isPending ? "pending" : isActive ? "active" : "")}
+        >
+          Drivers
+        </NavLink>
+        <NavLink
+          to="/teams"
+          className={({
+            isActive,
+            isPending,
+          }: {
+            isActive: boolean;
+            isPending: boolean;
+          }) => (isPending ? "pending" : isActive ? "active" : "")}
+        >
+          Teams
+        </NavLink>
+        <NavLink
+          to="/races"
+          className={({
+            isActive,
+            isPending,
+          }: {
+            isActive: boolean;
+            isPending: boolean;
+          }) => (isPending ? "pending" : isActive ? "active" : "")}
+        >
+          Races
+        </NavLink>
+      </nav>
+    </div>
+  );
 };
 
 export default SideBar;
