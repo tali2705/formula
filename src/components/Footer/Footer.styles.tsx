c0914885c6f3a028d2877eb51cf732fbb2a70006
Ferenc Koler <ferenckoler@outlook.com>
2024-06-06 22:20:24 +0200

    Styled Components #3

---
+++ src/components/Footer/Footer.styles.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+const bigFontColor = '#98aeb6';
+const footerBackgroundColor = '#9dc3d1';
+
+const FooterWrapper = styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 0.6em 0;
+    border-top: 1px solid ${bigFontColor};
+    text-align: center;
+    background-color: ${footerBackgroundColor};
+    box-shadow: 3px 3px 20px rgba(0, 0, 0, 0.3);
+
+    .footer-icons {
+        padding: 0 0.55em;
+        width: 1.2em;
+    }
+
+    sub,
+    sup {
+        font-size: 75%;
+        line-height: 0;
+        position: relative;
+        vertical-align: baseline;
+    }
+
+    sup {
+        top: -0.5em;
+    }
+
+    sub {
+        bottom: -0.25em;
+    }
+`;
+
+export default FooterWrapper;
